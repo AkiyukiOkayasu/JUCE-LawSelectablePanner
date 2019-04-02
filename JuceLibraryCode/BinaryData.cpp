59e6495ee0c4d58c5f82f55575d552e2b916a954
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2019-04-02 16:36:47 +0900

    Re-save all with JUCE v5.4.3

--- JuceLibraryCode/BinaryData.cpp
+++ JuceLibraryCode/BinaryData.cpp
@@ -84,11 +84,11 @@ static const unsigned char temp_binary_data_0[] =
 const char* Icon_AkiyukiOkayasu_png = (const char*) temp_binary_data_0;
 
 
-const char* getNamedResource (const char*, int&) throw();
-const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw()
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
-    if (resourceNameUTF8 != 0)
+
+    if (resourceNameUTF8 != nullptr)
         while (*resourceNameUTF8 != 0)
             hash = 31 * hash + (unsigned int) *resourceNameUTF8++;
 
@@ -99,12 +99,28 @@ const char* getNamedResource (const char* resourceNameUTF8, int& numBytes) throw
     }
 
     numBytes = 0;
-    return 0;
+    return nullptr;
 }
 
 const char* namedResourceList[] =
 {
     "Icon_AkiyukiOkayasu_png"
 };
+
+const char* originalFilenames[] =
+{
+    "Icon_AkiyukiOkayasu.png"
+};
+
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
+{
+    for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
+    {
+        if (namedResourceList[i] == resourceNameUTF8)
+            return originalFilenames[i];
+    }
+
+    return nullptr;
+}
 
 }
